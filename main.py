8f357218892662a59e7451696c7757d522636cd7
alokenveo <alokenveo@alumnos.unex.es>
2024-10-18 15:40:50 +0200

    Adición de todas las tareas, arreglos varios

--- main.py
+++ main.py
@@ -17,10 +17,56 @@ servidores = [
 
 # Lista de tareas
 tareas = [
-    {'id': 1, 'cpu': 6, 'memoria': 7, 'time': 25, 'priority': 'Baja'},
-    {'id': 2, 'cpu': 2, 'memoria': 5, 'time': 37, 'priority': 'Media'},
-    # Añade todas las tareas aquí...
-    {'id': 50, 'cpu': 1, 'memoria': 5, 'time': 73, 'priority': 'Baja'}
+    {'id': 1, 'cpu': 6, 'memoria': 7, 'time': 25, 'prioridad': 'Baja'},
+    {'id': 2, 'cpu': 2, 'memoria': 5, 'time': 37, 'prioridad': 'Media'},
+    {'id': 3, 'cpu': 6, 'memoria': 15, 'time': 44, 'prioridad': 'Media'},
+    {'id': 4, 'cpu': 1, 'memoria': 11, 'time': 5, 'prioridad': 'Media'},
+    {'id': 5, 'cpu': 3, 'memoria': 13, 'time': 6, 'prioridad': 'Alta'},
+    {'id': 6, 'cpu': 8, 'memoria': 8, 'time': 12, 'prioridad': 'Media'},
+    {'id': 7, 'cpu': 1, 'memoria': 15, 'time': 101, 'prioridad': 'Baja'},
+    {'id': 8, 'cpu': 8, 'memoria': 14, 'time': 101, 'prioridad': 'Baja'},
+    {'id': 9, 'cpu': 5, 'memoria': 11, 'time': 76, 'prioridad': 'Alta'},
+    {'id': 10, 'cpu': 5, 'memoria': 9, 'time': 68, 'prioridad': 'Alta'},
+    {'id': 11, 'cpu': 2, 'memoria': 4, 'time': 111, 'prioridad': 'Alta'},
+    {'id': 12, 'cpu': 6, 'memoria': 5, 'time': 54, 'prioridad': 'Media'},
+    {'id': 13, 'cpu': 3, 'memoria': 4, 'time': 31, 'prioridad': 'Media'},
+    {'id': 14, 'cpu': 3, 'memoria': 8, 'time': 79, 'prioridad': 'Baja'},
+    {'id': 15, 'cpu': 4, 'memoria': 9, 'time': 45, 'prioridad': 'Alta'},
+    {'id': 16, 'cpu': 7, 'memoria': 6, 'time': 21, 'prioridad': 'Media'},
+    {'id': 17, 'cpu': 3, 'memoria': 15, 'time': 97, 'prioridad': 'Media'},
+    {'id': 18, 'cpu': 5, 'memoria': 15, 'time': 20, 'prioridad': 'Media'},
+    {'id': 19, 'cpu': 8, 'memoria': 8, 'time': 68, 'prioridad': 'Alta'},
+    {'id': 20, 'cpu': 6, 'memoria': 9, 'time': 61, 'prioridad': 'Baja'},
+    {'id': 21, 'cpu': 5, 'memoria': 8, 'time': 74, 'prioridad': 'Baja'},
+    {'id': 22, 'cpu': 7, 'memoria': 7, 'time': 110, 'prioridad': 'Media'},
+    {'id': 23, 'cpu': 1, 'memoria': 4, 'time': 30, 'prioridad': 'Baja'},
+    {'id': 24, 'cpu': 2, 'memoria': 9, 'time': 27, 'prioridad': 'Baja'},
+    {'id': 25, 'cpu': 3, 'memoria': 12, 'time': 79, 'prioridad': 'Alta'},
+    {'id': 26, 'cpu': 1, 'memoria': 13, 'time': 79, 'prioridad': 'Baja'},
+    {'id': 27, 'cpu': 4, 'memoria': 3, 'time': 46, 'prioridad': 'Media'},
+    {'id': 28, 'cpu': 5, 'memoria': 3, 'time': 37, 'prioridad': 'Baja'},
+    {'id': 29, 'cpu': 5, 'memoria': 2, 'time': 43, 'prioridad': 'Alta'},
+    {'id': 30, 'cpu': 3, 'memoria': 15, 'time': 95, 'prioridad': 'Baja'},
+    {'id': 31, 'cpu': 4, 'memoria': 9, 'time': 45, 'prioridad': 'Baja'},
+    {'id': 32, 'cpu': 4, 'memoria': 5, 'time': 45, 'prioridad': 'Baja'},
+    {'id': 33, 'cpu': 5, 'memoria': 13, 'time': 108, 'prioridad': 'Alta'},
+    {'id': 34, 'cpu': 4, 'memoria': 4, 'time': 113, 'prioridad': 'Baja'},
+    {'id': 35, 'cpu': 8, 'memoria': 14, 'time': 116, 'prioridad': 'Media'},
+    {'id': 36, 'cpu': 2, 'memoria': 5, 'time': 92, 'prioridad': 'Baja'},
+    {'id': 37, 'cpu': 8, 'memoria': 7, 'time': 89, 'prioridad': 'Alta'},
+    {'id': 38, 'cpu': 3, 'memoria': 8, 'time': 39, 'prioridad': 'Alta'},
+    {'id': 39, 'cpu': 2, 'memoria': 16, 'time': 98, 'prioridad': 'Baja'},
+    {'id': 40, 'cpu': 2, 'memoria': 2, 'time': 8, 'prioridad': 'Media'},
+    {'id': 41, 'cpu': 3, 'memoria': 3, 'time': 65, 'prioridad': 'Media'},
+    {'id': 42, 'cpu': 4, 'memoria': 12, 'time': 70, 'prioridad': 'Media'},
+    {'id': 43, 'cpu': 5, 'memoria': 12, 'time': 16, 'prioridad': 'Media'},
+    {'id': 44, 'cpu': 4, 'memoria': 6, 'time': 65, 'prioridad': 'Media'},
+    {'id': 45, 'cpu': 2, 'memoria': 16, 'time': 30, 'prioridad': 'Baja'},
+    {'id': 46, 'cpu': 5, 'memoria': 3, 'time': 98, 'prioridad': 'Media'},
+    {'id': 47, 'cpu': 4, 'memoria': 5, 'time': 90, 'prioridad': 'Media'},
+    {'id': 48, 'cpu': 8, 'memoria': 15, 'time': 7, 'prioridad': 'Media'},
+    {'id': 49, 'cpu': 7, 'memoria': 12, 'time': 33, 'prioridad': 'Media'},
+    {'id': 50, 'cpu': 1, 'memoria': 5, 'time': 73, 'prioridad': 'Baja'}
 ]
 
 
@@ -29,8 +75,8 @@ tareas = [
 def inicializar_poblacion(pop_size, num_tareas, num_servers):
     poblacion = []
     for _ in range(pop_size):
-        individual = [random.randint(1, num_servers) for _ in range(num_tareas)]  # Asigna aleatoriamente servidores
-        poblacion.append(individual)
+        individuo = [random.randint(1, num_servers) for _ in range(num_tareas)]  # Asigna aleatoriamente servidores
+        poblacion.append(individuo)
     return poblacion
 
 
@@ -95,21 +141,21 @@ def algoritmo_genetico(pop_size, generations, tareas, servidores):
     poblacion = inicializar_poblacion(pop_size, len(tareas), len(servidores))
 
     for generacion in range(generations):
-        fitnesses = [fitness(ind, tareas, servidores) for ind in population]
+        fitnesses = [fitness(ind, tareas, servidores) for ind in poblacion]
         nueva_poblacion = []
 
         # Crear nuevos individuos a través de cruce y mutación
         for _ in range(pop_size // 2):
-            padre1 = ruleta(population, fitnesses)
-            padre2 = ruleta(population, fitnesses)
+            padre1 = ruleta(poblacion, fitnesses)
+            padre2 = ruleta(poblacion, fitnesses)
             descendiente1, descendiente2 = cruce(padre1, padre2)
             nueva_poblacion.extend([mutacion(descendiente1), mutacion(descendiente2)])
 
         # Reemplazar parte de la población
-        population = reemplazo(population, nueva_poblacion)
+        poblacion = reemplazo(poblacion, nueva_poblacion)
 
         # Evaluar mejor individuo
-        mejor_individuo = max(population, key=lambda ind: fitness(ind, tareas, servidores))
+        mejor_individuo = max(poblacion, key=lambda ind: fitness(ind, tareas, servidores))
         mejor_fitness = fitness(mejor_individuo, tareas, servidores)
         print(f"Generación {generacion}, Mejor fitness: {mejor_fitness}, Mejor solución: {mejor_individuo}")
 
